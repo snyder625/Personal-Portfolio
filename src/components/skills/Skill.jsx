09235dca5fff7ff194f27eeac85b72615da39c1e
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-09-25 19:00:00 +0500

    Data Updated

---
+++ src/components/skills/Skill.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Skill = () => {
+  return (
+        <div class="first-div">
+            <div class="outer-div">
+                <div class="inner-div">
+                    <h1> HTML </h1>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Skill
--- src/data.js
+++ src/data.js
@@ -1,28 +1,28 @@
 export const products = [
     {
         id: 1,
-        img: "https://cdn.dribbble.com/users/226518/screenshots/10776484/companywebsiteshowcase.png?compress=1&resize=400x300",
-        link: "snyder625.github.io/personalsite"
+        img: "https://firebasestorage.googleapis.com/v0/b/clone-86962.appspot.com/o/1664093763510screencapture-localhost-3000-home-2022-09-25-13_11_22.png?alt=media&token=5e60f7f0-cddd-4afc-adb4-791088eea640",
+        link: "https://github.com/snyder625/Social-Media-App-Complete"
     },
     {
         id: 2,
-        img: "https://cdn.dribbble.com/users/226518/screenshots/10776484/companywebsiteshowcase.png?compress=1&resize=400x300",
-        link: "https://snyder625.github.io/personalsite/"
+        img: "https://firebasestorage.googleapis.com/v0/b/clone-86962.appspot.com/o/1664093987941screencapture-localhost-3000-2022-09-25-13_18_42.png?alt=media&token=d52cc4d8-b5de-4cdb-8ac8-20dfc2ef4c43",
+        link: "https://github.com/snyder625/Youtube-Clone"
     },
     {
         id: 3,
-        img: '../src/img/tindog.png',
+        img: "https://firebasestorage.googleapis.com/v0/b/clone-86962.appspot.com/o/1664092936126tindog.png?alt=media&token=ab069944-280f-4c6b-8da6-a67db1294bc2",
         link: "https://snyder625.github.io/tindog/"
     },
     {
         id: 4,
-        img: "https://cdn.dribbble.com/users/226518/screenshots/10776484/companywebsiteshowcase.png?compress=1&resize=400x300",
-        link: "snyder625.github.io/personalsite"
+        img: "https://firebasestorage.googleapis.com/v0/b/clone-86962.appspot.com/o/1664093096901barbera.png?alt=media&token=0a20890f-ab75-4fa3-8f7d-5b7dc5148b00",
+        link: "https://github.com/snyder625/DBMS-Barberah"
     },
     {
         id: 5,
         img: "https://cdn.dribbble.com/users/226518/screenshots/10776484/companywebsiteshowcase.png?compress=1&resize=400x300",
-        link: "snyder625.github.io/personalsite"
+        link: "https://snyder625.github.io/Softsolutions/"
     },
     {
         id: 6,
